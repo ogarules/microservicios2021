b6fa645217c91f4ec7b2a235f3ceda1724acacf5
Ogarules <oscargl2017@hotmail.com>
2021-04-10 16:18:08 -0500

    Actualizacion de JPA con prueba sunitarias

--- JPA/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ JPA/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-@SpringBootTest
+import com.example.demo.models.Pet;
+import com.example.demo.repositories.PetRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.var;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+classes = DemoApplication.class)
 class DemoApplicationTests {
 
+	@Autowired
+    PetRepository respository;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		var pet = new Pet();
+		pet.setName("banana");
+		pet.setTag("ASDF-234");
+
+		assertNull(pet.getId());
+
+		this.respository.save(pet);
+
+		assertNotNull(pet.getId());
 	}
 
 }
